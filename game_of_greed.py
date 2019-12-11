fc6ba0e437eb49cd3650f2646d4cb1dd0f38d023
Christopher Ceder <cceder@ChristophersAir.localdomain>
2019-12-11 10:11:14 -0800

    scoring working

--- game_of_greed.py
+++ game_of_greed.py
@@ -6,9 +6,9 @@ class Game:
 
     def __init__(self):
         pass
-        # self.name = name
 
     def play(self):
+
         print('welcome to game of greed!')
 
         while True:
@@ -20,12 +20,51 @@ class Game:
                 print('ok. maybe another time')
                 break
 
-    def calculate_score(self)
+    def calculate_score(self, dice):
+        
+        roll_counter = Counter(dice)
+        while True:
+            score_count = 0
+            if len(roll_counter) > 0:
+                if len(roll_counter) == 6:
+                    return 1500
+                elif len(roll_counter) == 3 and roll_counter.most_common()[2][1] == 2:
+                    return 1500
+                for key, value in roll_counter.items():
+                    if roll_counter[5] == 4 and roll_counter[1] == 2:
+                        score_count += 2000        
+                    elif key == 1 and value == 6:
+                        score_count += 4000
+                    elif key == 1 and value == 5:
+                        score_count += 3000
+                    elif key == 1 and value == 4:
+                        score_count += 2000
+                    elif key == 1 and value == 3:
+                        score_count += 1000
+                    elif key >= 2 and value == 3:
+                        score_count += (key)*100
+                    elif key >= 2 and value == 4:
+                        score_count += (key)*200
+                    elif key >= 2 and value == 5:
+                        score_count += (key)*300
+                    elif key >= 2 and value == 6:
+                        score_count += (key)*400
+                    elif key == 1 and value <= 2:
+                        score_count += (value)*100
+                    elif key == 5 and value < 3:
+                        score_count += (value)*50
+                roll_counter.popitem()
+            else:
+                break 
+            return score_count 
+        return 0
 
 if __name__ == "__main__":
-    pl = Game()
+    game = Game()
 
     try:
-        pl.play()
+        game.play()
+        game.calculate_score(dice)
+
     except:
         print('well then')
